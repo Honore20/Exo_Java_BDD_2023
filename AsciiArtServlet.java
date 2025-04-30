c2659a914817c2426f19f4d0af6ea615c686e8a0
Honore20 <avekorm@gmail.com>
2025-04-30 11:11:07 +0200

    Update AsciiArtServlet.java

--- AsciiArtServlet.java
+++ AsciiArtServlet.java
@@ -1,53 +1,31 @@
 package com.example;
 
-import java.io.*;
+import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class AsciiArtServlet extends HttpServlet {
-
-    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
-
-    private static final int WIDTH = 4;  // Par exemple
-    private static final int HEIGHT = 5;
-    
-    private static final String[] ASCII_FONT = {
-        " A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z  ? ",
-        "/|  |\\ |\\ |\\ |-- |-- |-- |\\ |\\  |  |\\ |\\ |\\ |\\ |\\ |\\ |-- |-- |-- |-- |\\ |\\ |\\ |  ",
-        " |--|  | | | |-- |-- |\\  | |\\ |\\ |\\ | | | |\\ | | | | |\\ | | | |\\ | | | |\\ |\\ |\\ ",
-        " |  |\\ |/ |/ |__ |__ | \\ | | \\|/ | \\|/ |/ | \\|/ |/ |/ | | |/ |/ |/ |/ |/ |/ |/  ",
-        "                                        (exemple fixe, à modifier si besoin)"
-    };
-
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String lettre = request.getParameter("lettre");
+        String resultat = "";
 
-        String text = request.getParameter("chaine");
-        if (text == null || text.length() < 1) {
-            request.setAttribute("asciiResult", null);
-            request.getRequestDispatcher("/ascii.jsp").forward(request, response);
-            return;
-        }
+        if (lettre != null && lettre.length() == 1) {
+            char ch = lettre.charAt(0);
+            resultat = "Lettre reçue : " + ch + "<br/>";
 
-        text = text.toUpperCase();
-        StringBuilder[] resultLines = new StringBuilder[HEIGHT];
-        for (int i = 0; i < HEIGHT; i++) {
-            resultLines[i] = new StringBuilder();
-        }
-
-        for (char c : text.toCharArray()) {
-            int index = ALPHABET.indexOf(c);
-            if (index == -1) index = ALPHABET.length() - 1; // '?'
-
-            for (int h = 0; h < HEIGHT; h++) {
-                int start = index * WIDTH;
-                resultLines[h].append(ASCII_FONT[h], start, start + WIDTH);
+            // Exemple simple ASCII art
+            if (ch == 'A' || ch == 'a') {
+                resultat += "&nbsp;&nbsp;A<br/> A&nbsp;&nbsp;A<br/>AAAAA<br/>A&nbsp;&nbsp;A<br/>";
+            } else {
+                resultat += "Pas d'ASCII art disponible pour cette lettre.";
             }
+        } else {
+            resultat = "Veuillez entrer une seule lettre.";
         }
 
-        request.setAttribute("asciiResult", resultLines);
-        request.setAttribute("originalText", text);
-        request.getRequestDispatcher("/ascii.jsp").forward(request, response);
+        request.setAttribute("ascii", resultat);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("ascii.jsp");
+        dispatcher.forward(request, response);
     }
 }
